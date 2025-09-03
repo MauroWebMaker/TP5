620c4ff219209a05ae2c7a392259cc43e28462a2
Facü <facundolc08@gmail.com>
2025-09-03 10:36:29 -0300

    Creación del proyecto
    + carpetas + archivos base.
    
    Co-Authored-By: Agustin Mazza <agus-mazza@hotmail.com>
    Co-Authored-By: HYJeremias <226197862+Ego572@users.noreply.github.com>
    Co-Authored-By: Juan Cruz Rodriguez <juancruzrodriguez0@gmail.com>
    Co-Authored-By: Mauricio Damián Reta <mauricio.reta.03@gmail.com>
    Co-Authored-By: jeremiassflores <227914104+jeremiassflores@users.noreply.github.com>

---
+++ TP5/src/TP5xd/Main.java
@@ -0,0 +1,16 @@
+package TP5xd;
+
+/* @author Facundo Lopez Cozzella */
+
+public class Main {
+
+    public static void main(String[] args) {
+        // creación de objetos
+    
+    
+        // "cuerpo" del main
+    
+    
+    }
+
+}
