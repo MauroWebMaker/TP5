4f0458f629e10a0246acfef27803312f4eccd35a
jeremiassflores <jeremiassflores@gmail.com>
2025-09-10 22:22:29 -0300

    ahi le sume lo que le faltaba a agus
    listo el ver telefono por apellido. Pero no me corre

--- src/TP5/JIfrmBuscarTelefonoXApellido.java
+++ src/TP5/JIfrmBuscarTelefonoXApellido.java
@@ -2,6 +2,7 @@ package TP5;
 
 import java.util.Map;
 import java.awt.event.ItemEvent;
+import javax.swing.DefaultListModel;
 import javax.swing.table.DefaultTableModel;
 
 /*
@@ -10,10 +11,12 @@ import javax.swing.table.DefaultTableModel;
 public class JIfrmBuscarTelefonoXApellido extends javax.swing.JInternalFrame {
 
     private DefaultTableModel tablita = new DefaultTableModel();
+    private DefaultListModel<String> listita = new DefaultListModel<>();
 
     public JIfrmBuscarTelefonoXApellido() {
         initComponents();
         armarCabeceraApellido();
+        ListaApellido.setModel(listita);
     }
 
     @SuppressWarnings("unchecked")
@@ -22,11 +25,12 @@ public class JIfrmBuscarTelefonoXApellido extends javax.swing.JInternalFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
-        listaApellido = new javax.swing.JComboBox<>();
-        jButton2 = new javax.swing.JButton();
+        Botonsalir = new javax.swing.JButton();
         jScrollPane2 = new javax.swing.JScrollPane();
         TablaApellido = new javax.swing.JTable();
-        apellidooo = new javax.swing.JTextField();
+        apellidoo = new javax.swing.JTextField();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        ListaApellido = new javax.swing.JList<>();
 
         jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
         jLabel1.setText("Buscar Telefono por Apellido");
@@ -34,16 +38,10 @@ public class JIfrmBuscarTelefonoXApellido extends javax.swing.JInternalFrame {
         jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
         jLabel2.setText("Apellido:");
 
-        listaApellido.addItemListener(new java.awt.event.ItemListener() {
-            public void itemStateChanged(java.awt.event.ItemEvent evt) {
-                listaApellidoItemStateChanged(evt);
-            }
-        });
-
-        jButton2.setText("Salir");
-        jButton2.addActionListener(new java.awt.event.ActionListener() {
+        Botonsalir.setText("Salir");
+        Botonsalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton2ActionPerformed(evt);
+                BotonsalirActionPerformed(evt);
             }
         });
 
@@ -60,29 +58,31 @@ public class JIfrmBuscarTelefonoXApellido extends javax.swing.JInternalFrame {
         ));
         jScrollPane2.setViewportView(TablaApellido);
 
-        apellidooo.addKeyListener(new java.awt.event.KeyAdapter() {
+        apellidoo.addKeyListener(new java.awt.event.KeyAdapter() {
             public void keyReleased(java.awt.event.KeyEvent evt) {
-                apellidoooKeyReleased(evt);
+                apellidooKeyReleased(evt);
             }
         });
+
+        jScrollPane1.setViewportView(ListaApellido);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addContainerGap(15, Short.MAX_VALUE)
+                .addContainerGap(44, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jButton2)
+                        .addComponent(Botonsalir)
                         .addGap(54, 54, 54))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(listaApellido, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
                             .addGroup(layout.createSequentialGroup()
                                 .addComponent(jLabel2)
                                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                                .addComponent(apellidooo, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                .addComponent(apellidoo, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE))
                         .addGap(18, 18, 18)
                         .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 436, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(54, 54, 54))
@@ -96,51 +96,63 @@ public class JIfrmBuscarTelefonoXApellido extends javax.swing.JInternalFrame {
                 .addGap(14, 14, 14)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                             .addComponent(jLabel2)
-                            .addComponent(apellidooo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(listaApellido, javax.swing.GroupLayout.PREFERRED_SIZE, 117, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(34, 34, 34)
-                .addComponent(jButton2)
+                            .addComponent(apellidoo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(7, 7, 7))
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(34, 34, 34)))
+                .addComponent(Botonsalir)
                 .addContainerGap(32, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+    private void BotonsalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BotonsalirActionPerformed
         this.dispose();
-    }//GEN-LAST:event_jButton2ActionPerformed
+    }//GEN-LAST:event_BotonsalirActionPerformed
 
-    private void listaApellidoItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_listaApellidoItemStateChanged
-        // TODO add your handling code here:
-    }//GEN-LAST:event_listaApellidoItemStateChanged
-
-    private void apellidoooKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_apellidoooKeyReleased
+    private void apellidooKeyReleased(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_apellidooKeyReleased
         tablita.setRowCount(0);
         for (Map.Entry<Long, Cliente> auxi : JfrmMenuPrincipal.directorio.entrySet()) {
             Cliente auxiC = auxi.getValue();
             Long auxiL = auxi.getKey();
 
-            if (auxiC.getApellido().toLowerCase().startsWith(apellidooo.getText().toLowerCase())) {
+            if (auxiC.getApellido().toLowerCase().startsWith(apellidoo.getText().toLowerCase())) {
                 tablita.addRow(new Object[]{auxiC.getDNI(), auxiC.getApellido(), auxiC.getNombre(), auxiC.getDireccion(), auxiC.getCiudad(), auxiL});
             }
         }
-    }//GEN-LAST:event_apellidoooKeyReleased
+        
+        listita.clear();
+        for (Map.Entry<Long, Cliente> auxi : JfrmMenuPrincipal.directorio.entrySet()) {
+            Cliente auxiC = auxi.getValue();
+
+            String apellidoIngresado = apellidoo.getText();
+            String apellidoCliente = String.valueOf(auxiC.getApellido());
+            if (apellidoCliente.startsWith(apellidoIngresado)) {
+                listita.addElement(apellidoCliente);
+            }
+        }
+        
+        
+    }//GEN-LAST:event_apellidooKeyReleased
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton Botonsalir;
+    private javax.swing.JList<String> ListaApellido;
     private javax.swing.JTable TablaApellido;
-    private javax.swing.JTextField apellidooo;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JTextField apellidoo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JComboBox<String> listaApellido;
     // End of variables declaration//GEN-END:variables
 
     private void armarCabeceraApellido() {
